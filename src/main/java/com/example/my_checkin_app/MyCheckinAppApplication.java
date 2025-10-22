af6ea5c15576806b91e7a85ff5350eff30a7bd0c
MelonCaully <maloneingham@gmail.com>
2025-10-22 14:42:56 -0400

    initial commit

---
+++ src/main/java/com/example/my_checkin_app/MyCheckinAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_checkin_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCheckinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCheckinAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/my_checkin_app/MyCheckinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_checkin_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCheckinAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
